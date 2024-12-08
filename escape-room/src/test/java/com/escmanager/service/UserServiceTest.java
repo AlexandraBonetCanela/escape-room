30e7dcb6b002931de74e54adac64a920fcf098b9
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-12-08 20:14:16 +0100

    WIP: UserService testing

--- escape-room/src/test/java/com/escmanager/service/UserServiceTest.java
+++ escape-room/src/test/java/com/escmanager/service/UserServiceTest.java
@@ -1,4 +1,44 @@
+package com.escmanager.service;
+
+import com.escmanager.dao.UserDAO;
+import com.escmanager.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-  
+
+    @Mock
+    private UserDAO userDAO;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void addUser_UserDoesNotExists() {
+
+        String email = "jose@gmail.com";
+        when(userDAO.findByEmail(email)).thenReturn(null);
+
+        User createUser = new User();
+        createUser.setEmail(email);
+        createUser.setId(1);
+        when(userDAO.create(any(User.class))).thenReturn(createUser);
+
+        User result = userService.addUser(email);
+
+        assertNotNull(result);
+        assertEquals(result.getEmail(), email);
+        assertEquals(result.getId(), 1);
+        verify(userDAO).findByEmail(email);
+        verify(userDAO).create(any(User.class));
+    }
 }
