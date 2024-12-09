2134da88c2dc2077725b1aa2873d3f1185a0e217
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-12-09 12:58:44 +0100

    added tests

--- escape-room/src/test/java/com/escmanager/service/UserServiceTest.java
+++ escape-room/src/test/java/com/escmanager/service/UserServiceTest.java
@@ -10,8 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -23,7 +22,7 @@ class UserServiceTest {
     private UserService userService;
 
     @Test
-    void addUser_UserDoesNotExists() {
+    void test_addUser_UserDoesNotExists() {
 
         String email = "jose@gmail.com";
         when(userDAO.findByEmail(email)).thenReturn(null);
@@ -40,5 +39,69 @@ class UserServiceTest {
         assertEquals(result.getId(), 1);
         verify(userDAO).findByEmail(email);
         verify(userDAO).create(any(User.class));
+    }
+
+    @Test
+    void test_addUser_UserAlreadyExists() {
+
+        String email = "jose@gmail.com";
+        User existingUser = new User();
+        existingUser.setEmail(email);
+        when(userDAO.findByEmail(email)).thenReturn(existingUser);
+
+        User result = userService.addUser(email);
+
+        assertNotNull(result);
+        assertEquals(result.getEmail(), email);
+        verify(userDAO).findByEmail(email);
+        verify(userDAO, never()).create(any(User.class));
+    }
+
+    @Test
+    void test_registerUser_UserDoesNotExists() {
+        String email = "jose@gmail.com";
+        String name = "Jose Manuel";
+        when(userDAO.findByEmail(email)).thenReturn(null);
+
+        User createUser = new User();
+        createUser.setEmail(email);
+        createUser.setName(name);
+        createUser.setRegistered(true);
+        when(userDAO.create(any(User.class))).thenReturn(createUser);
+
+        User result = userService.registerUser(email, name);
+
+        assertNotNull(result);
+        assertEquals(result.getEmail(), email);
+        assertEquals(result.getName(), name);
+        assertTrue(result.isRegistered());
+        verify(userDAO).findByEmail(email);
+        verify(userDAO).create(any(User.class));
+    }
+
+    @Test
+    void test_registerUser_UserAlreadyExists() {
+        String email = "jose@gmail.com";
+        String name = "Jose Manuel";
+
+        User existingUser = new User();
+        existingUser.setEmail(email);
+
+        when(userDAO.findByEmail(email)).thenReturn(existingUser);
+
+        User registeredUser = new User();
+        registeredUser.setEmail(email);
+        registeredUser.setName(name);
+        registeredUser.setRegistered(true);
+        when(userDAO.update(any(User.class))).thenReturn(registeredUser);
+
+        User result = userService.registerUser(email, name);
+
+        assertNotNull(result);
+        assertEquals(result.getEmail(), email);
+        assertEquals(result.getName(), name);
+        assertTrue(result.isRegistered());
+        verify(userDAO).findByEmail(email);
+        verify(userDAO).update(any(User.class));
     }
 }
