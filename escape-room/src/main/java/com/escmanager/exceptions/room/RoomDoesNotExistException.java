ef09c0e5e7b04fe64c8595a084116af4781aae13
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-12-11 12:20:14 +0100

    wip

--- escape-room/src/main/java/com/escmanager/exceptions/room/RoomDoesNotExistException.java
+++ escape-room/src/main/java/com/escmanager/exceptions/room/RoomDoesNotExistException.java
@@ -1,7 +1,7 @@
 package com.escmanager.exceptions.room;
 
 public class RoomDoesNotExistException extends Exception{
-    
+
     public RoomDoesNotExistException(String message){
         super(message);
     }
