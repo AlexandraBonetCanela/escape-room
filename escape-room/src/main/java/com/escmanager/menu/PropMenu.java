83db4b584ced1e7cc77f0df206231e5007872281
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-12-17 07:49:22 +0100

    added back accidentally deleted menu

--- escape-room/src/main/java/com/escmanager/menu/PropMenu.java
+++ escape-room/src/main/java/com/escmanager/menu/PropMenu.java
@@ -1,51 +1,51 @@
 package com.escmanager.menu;
 
-//import com.escmanager.service.PropService;
+import com.escmanager.service.PropService;
 import com.escmanager.exceptions.RoomNotFoundException;
 import static com.escmanager.menu.Main.scanner;
-//import static com.escmanager.menu.Main.propService;
+import static com.escmanager.menu.Main.propService;
 
 public class PropMenu {
     public static void showMenu() {
         boolean backToMain = false;
-//        while (!backToMain) {
-//            System.out.println("""
-//                    Prop Management. Choose one of the options:
-//                    1. Add Prop to Room
-//                    2. Delete Prop from Room
-//                    3. Show Props in a Room
-//                    4. Back to Main Menu
-//                    """);
-//            int option = scanner.nextInt();
-//            scanner.nextLine();
-//            try {
-//                switch (option) {
-//                    case 1 -> {
-//                        System.out.print("Enter Room ID: ");
-//                        int roomId = scanner.nextInt();
-//                        scanner.nextLine();
-//                        System.out.print("Enter Prop Name: ");
-//                        String propName = scanner.nextLine();
-//                        propService.addProp(roomId, propName);
-//                    }
-//                    case 2 -> {
-//                        System.out.print("Enter Room ID: ");
-//                        int roomId = scanner.nextInt();
-//                        System.out.print("Enter Prop ID to delete: ");
-//                        int propId = scanner.nextInt();
-//                        propService.deleteProp(roomId, propId);
-//                    }
-//                    case 3 -> {
-//                        System.out.print("Enter Room ID: ");
-//                        int roomId = scanner.nextInt();
-//                        propService.showProps(roomId);
-//                    }
-//                    case 4 -> backToMain = true;
-//                    default -> System.out.println("Invalid choice. Returning to main menu.");
-//                }
-//            } catch (RoomNotFoundException e) {
-//                System.out.println("Error: " + e.getMessage());
-//            }
-//        }
+        while (!backToMain) {
+            System.out.println("""
+                    Prop Management. Choose one of the options:
+                    1. Add Prop to Room
+                    2. Delete Prop from Room
+                    3. Show Props in a Room
+                    4. Back to Main Menu
+                    """);
+            int option = scanner.nextInt();
+            scanner.nextLine();
+            try {
+                switch (option) {
+                    case 1 -> {
+                        System.out.print("Enter Room ID: ");
+                        int roomId = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.print("Enter Prop Name: ");
+                        String propName = scanner.nextLine();
+                        propService.addProp(roomId, propName);
+                    }
+                    case 2 -> {
+                        System.out.print("Enter Room ID: ");
+                        int roomId = scanner.nextInt();
+                        System.out.print("Enter Prop ID to delete: ");
+                        int propId = scanner.nextInt();
+                        propService.deleteProp(roomId, propId);
+                    }
+                    case 3 -> {
+                        System.out.print("Enter Room ID: ");
+                        int roomId = scanner.nextInt();
+                        propService.showProps(roomId);
+                    }
+                    case 4 -> backToMain = true;
+                    default -> System.out.println("Invalid choice. Returning to main menu.");
+                }
+            } catch (RoomNotFoundException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
     }
 }
--- escape-room/src/main/java/com/escmanager/menu/UserMenu.java
+++ escape-room/src/main/java/com/escmanager/menu/UserMenu.java
@@ -1,38 +1,38 @@
 package com.escmanager.menu;
 
-//import com.escmanager.service.UserService;
+import com.escmanager.service.UserService;
 import static com.escmanager.menu.Main.scanner;
-//import static com.escmanager.menu.Main.userService;
+import static com.escmanager.menu.Main.userService;
 
 public class UserMenu {
     public static void showMenu() {
-//        boolean backToMain = false;
-//        while (!backToMain) {
-//            System.out.println("""
-//                    User Management. Choose one of the options:
-//                    1. Add User
-//                    2. Delete User
-//                    3. List All Users
-//                    4. Back to Main Menu
-//                    """);
-//            int option = scanner.nextInt();
-//            scanner.nextLine();
-//
-//            switch (option) {
-//                case 1 -> {
-//                    System.out.print("Enter User Name: ");
-//                    String userName = scanner.nextLine();
-//                    userService.addUser(userName);
-//                }
-//                case 2 -> {
-//                    System.out.print("Enter User ID to delete: ");
-//                    int userId = scanner.nextInt();
-//                    userService.deleteUser(userId);
-//                }
-//                case 3 -> userService.listUsers();
-//                case 4 -> backToMain = true;
-//                default -> System.out.println("Invalid choice. Returning to main menu.");
-//            }
-//        }
+        boolean backToMain = false;
+        while (!backToMain) {
+            System.out.println("""
+                    User Management. Choose one of the options:
+                    1. Add User
+                    2. Delete User
+                    3. List All Users
+                    4. Back to Main Menu
+                    """);
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1 -> {
+                    System.out.print("Enter User Name: ");
+                    String userName = scanner.nextLine();
+                    userService.addUser(userName);
+                }
+                case 2 -> {
+                    System.out.print("Enter User ID to delete: ");
+                    int userId = scanner.nextInt();
+                    userService.deleteUser(userId);
+                }
+                case 3 -> userService.listUsers();
+                case 4 -> backToMain = true;
+                default -> System.out.println("Invalid choice. Returning to main menu.");
+            }
+        }
     }
 }
