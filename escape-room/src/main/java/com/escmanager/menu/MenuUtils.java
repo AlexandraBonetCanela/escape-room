9d620b6aa4bf858b92bbcd30aa2e4ce9c9c60854
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-12-18 21:13:24 +0100

    added print all rooms and print all Escaperooms

--- escape-room/src/main/java/com/escmanager/menu/MenuUtils.java
+++ escape-room/src/main/java/com/escmanager/menu/MenuUtils.java
@@ -1,6 +1,13 @@
 package com.escmanager.menu;
 
+import com.escmanager.model.EscapeRoom;
+import com.escmanager.model.Room;
+
+import java.util.List;
+
+import static com.escmanager.menu.EscapeRoomMenu.escapeRoomService;
 import static com.escmanager.menu.Main.scanner;
+import static com.escmanager.menu.RoomMenu.roomService;
 
 public class MenuUtils {
     public static String getNonEmptyString(String propertyName) {
@@ -14,5 +21,17 @@ public class MenuUtils {
         }
 
         return result;
+    }
+
+    public static void showAllRooms(){
+        System.out.println("Select a room: ");
+        List<Room> roomList = roomService.getAllRooms();
+        RoomMenu.printRooms(roomList);
+    }
+
+    public static void showAllEscapeRooms(){
+        System.out.println("Current Escape Rooms:");
+        List<EscapeRoom> escapeRoomList = escapeRoomService.getAllEscapeRooms();
+        EscapeRoomMenu.printEscapeRooms(escapeRoomList);
     }
 }
