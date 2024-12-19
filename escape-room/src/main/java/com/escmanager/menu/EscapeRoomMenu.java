73316f6dcb487d8e38a0323e9bc59d13db7ff5f2
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-12-19 08:16:19 +0100

    deleted old inventory

--- escape-room/src/main/java/com/escmanager/menu/EscapeRoomMenu.java
+++ escape-room/src/main/java/com/escmanager/menu/EscapeRoomMenu.java
@@ -2,15 +2,11 @@ package com.escmanager.menu;
 
 import com.escmanager.exceptions.escaperoom.EscapeRoomAlreadyExistException;
 import com.escmanager.exceptions.escaperoom.EscapeRoomDoesNotExistException;
-import com.escmanager.exceptions.room.RoomDoesNotExistException;
-import com.escmanager.model.*;
-import com.escmanager.service.ElementService;
 import com.escmanager.service.EscapeRoomService;
-import com.escmanager.service.RoomService;
+
 import static com.escmanager.menu.MenuUtils.getNonEmptyString;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 import static com.escmanager.menu.Menu.scanner;
 
@@ -59,82 +55,6 @@ public class EscapeRoomMenu {
             } catch (EscapeRoomDoesNotExistException | EscapeRoomAlreadyExistException e) {
                 System.out.println("Error: " + e.getMessage());
             }
-        }
-    }
-
-    private static void printInventory(int escaperoomId) {
-        System.out.println("\n");
-        System.out.println("Escape room info:");
-        EscapeRoom escapeRoom = escapeRoomService.getById(escaperoomId);
-        System.out.println("\t- ID:\t" + escapeRoom.getId());
-        System.out.println("\t- Name:\t" + escapeRoom.getName());
-        System.out.println("\t- Price:\t" + escapeRoom.getPrice());
-        System.out.println();
-
-        List<Room> rooms = RoomService.getInstance().findAllByEscaperoomId(escaperoomId);
-        for(Room room: rooms) {
-            System.out.println("Room info:");
-            System.out.println("\t- ID:\t" + room.getId());
-            System.out.println("\t- Name:\t" + room.getName());
-            System.out.println("\t- Difficulty:\t" + room.getDifficulty());
-            System.out.println("\t- Theme:\t" + room.getTheme());
-            System.out.println("\t- Total elements:\t" + room.getElementQuantity());
-
-            System.out.println("\t- Hints:");
-            try {
-                List<Hint> hints = ElementService.getInstance().getHints(room.getId());
-                if(hints.isEmpty()) {
-                    System.out.println("\t\t(No hints)");
-                }
-                else {
-                    System.out.println("\t\tID\tPrice\tName\tTheme");
-                }
-                for(Hint hint: hints) {
-                    System.out.print("\t\t");
-                    System.out.print(hint.getId());
-                    System.out.print("\t");
-                    System.out.print(hint.getPrice());
-                    System.out.print("\t");
-                    System.out.print(hint.getName());
-                    System.out.print("\t");
-                    System.out.print(hint.getTheme());
-                    System.out.println();
-                }
-
-                List<Prop> props = ElementService.getInstance().getProps(room.getId());
-                System.out.println("\t- Props:");
-
-                if(props.isEmpty()) {
-                    System.out.println("\t\t(No props)");
-                }
-                else {
-                    System.out.println("\t\tID\tPrice\tName\tMaterial type");
-                }
-                for(Prop prop: props) {
-                    System.out.print("\t\t");
-                    System.out.print(prop.getId());
-                    System.out.print("\t");
-                    System.out.print(prop.getPrice());
-                    System.out.print("\t");
-                    System.out.print(prop.getName());
-                    System.out.print("\t");
-                    System.out.print(prop.getMaterialType());
-                    System.out.println();
-                }
-
-            } catch (RoomDoesNotExistException e) {
-                System.out.println("The room does not exist!");
-            }
-        }
-
-        System.out.println("Escape room info:");
-        System.out.println("Escape room info:");
-        System.out.println("Escape room info:");
-    }
-
-    public static void printEscapeRooms(List<EscapeRoom> escapeRoomList) {
-        for (EscapeRoom escapeRooms : escapeRoomList){
-            System.out.println(escapeRooms);
         }
     }
 }
